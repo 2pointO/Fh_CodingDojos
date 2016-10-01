858d5bfa42c0be547bd78f71b84b7d34d20e3bdd
Sarah - 2pointO <wi15b073@technikum-wien.at>
2016-10-01 10:17:45 +0200

    adding shape libary and shape class properties

---
+++ Shapes/Shape.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Shapes
+{
+    public abstract class Shape
+    {
+
+        public Point Position { get; set; }
+
+        public abstract double CalculateArea();
+        public abstract double CalculatePerimeter();
+
+        public abstract void Draw();
+
+    }
+}
