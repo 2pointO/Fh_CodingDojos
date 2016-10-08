f94e2c02fc0002c50373389152a5976fb1fbfce8
Sarah - 2pointO <wi15b073@technikum-wien.at>
2016-10-08 16:53:32 +0200

    CD3 GUI added

--- CodingDojo3/MainWindow.xaml.cs
+++ CodingDojo3/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace CodingDojo3
         {
             InitializeComponent();
         }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
